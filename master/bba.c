a2d545609f1a26fd657d65748d2887569b4d0188
Chih-chieh Chang <ccch.realtouch@gmail.com>
2017-07-25 15:57:29 +0800

    modify bba.c, add new file sub_f2.txt

--- master/bba.c
+++ master/bba.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, and good-bye!");
+    printf("Hello, and good-bye!\n");
+    
+    // 9x9 multiply
+    int i;
+    for (i = 1; i <= 9; i ++) {
+        int j;
+        for (j = 1; j <= 9; j ++) {
+            printf("%dx%d=%d ", i, j, (i*j));
+        }
+        printf("\n");
+    }
 
     return 0;
 }
